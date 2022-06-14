03b37eff08e93d0d8b000da661b499e29e4a91b6
Jinyoung Jang <jyjang@uengine.org>
2022-06-14 06:52:21 +0000

    changes

--- gateway/src/main/java/com/example/template/Application.java
+++ gateway/src/main/java/com/example/template/Application.java
@@ -19,45 +19,6 @@ public class Application {
     public static ApplicationContext applicationContext;
     public static void main(String[] args) throws Exception{
 
-        // Dependency Inversion
-
-
-        // Dynamic casting
-        boolean drunk = true;
-        
-
-        Pet somePet = new Human();   
-        if(drunk)
-            somePet = new Dog();
-
-        System.out.println(somePet.getClass());
-        
-
-        // Standardized Batch processing //polymorphism
-        List<Pet> pets = new ArrayList<Pet>();
-        pets.add(new Cat());
-        pets.add(new Dog());
-
-        boolean mysqlIsPreferred = true;
-
-        Repository repository = (mysqlIsPreferred ? new MySQLRepositoryImpl() : new OracleRepositoryImpl());   //dynamic casting // dependency inversion
-
-        int newHealth = 100;
-
-        pets.forEach(pet ->{
-            System.out.println("건강 " + pet.getHealth());
-            pet.repository = repository;
-            pet.feed();
-
-            
-//            if(newHealth > 90) throw new RuntimeException("health 는 90 넘으면 안됨");  // Anemic domain model
-            pet.setHealth(100);
-
-            System.out.println("건강 " + pet.getHealth());
-
-            pet.save();
-        });
-
 
        applicationContext = SpringApplication.run(Application.class, args);
     }
